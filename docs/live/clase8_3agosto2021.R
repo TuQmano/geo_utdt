195a7289e53a3af936866b6d1d518e056a924531
TuQmano-Lenovo <juanpabloruiznicolini@gmail.com>
2021-08-03 17:55:25 -0300

    add class 8 link in site

---
+++ docs/live/clase8_3agosto2021.R
@@ -0,0 +1,13 @@
+# CLASE 6
+# GEO UTDT
+# Juan Pablo Ruiz Nicolini
+# 3 agosto 2021
+
+
+s <- livecode::serve_file()
+
+
+### 
+
+source("sesiones/scripts/obtener_tc_data.R")
+
---
+++ docs/sesiones/scripts/obtener_tc_data.R
@@ -0,0 +1,15 @@
+obtener_tc_data <- function(){
+  
+  #librerias necesarias
+  library(rvest)
+  library(tidyverse)
+  
+  # descargo html compelto
+  cronista <- read_html("https://www.cronista.com/MercadosOnline/dolar.html")
+  
+  # "raspo info en formato txt"
+  cronista %>% 
+    html_elements("#market-scrll-2 li") %>%
+    html_text2()
+  
+}
