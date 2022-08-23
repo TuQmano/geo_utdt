c93af56aad633b83fe5759694e0e6ed0a58b3125
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2022-08-23 18:31:25 -0300

    agrego clase 9

---
+++ docs/live/2022/clase9.R
@@ -0,0 +1,29 @@
+# CARGO LIBERARIAS
+
+library(tidyverse) # Easily Install and Load the 'Tidyverse'
+library(vroom) # Read and Write Rectangular Text Data Quickly
+library(geoAr) # Argentina's Spatial Data Toolbox
+library(geofacet) # 'ggplot2' Faceting Utilities for Geographical Data
+library(sf) # Simple Features for R
+library(patchwork) # The Composer of Plots, CRAN v1.1.1 
+
+
+# ACCEDO A DATOS ####
+
+## 1. DATOS ####
+
+
+
+## 2.GEOMETRIAS ####
+
+##### MAPA BASICO
+
+
+#### COMPARACION CON GRILLA
+
+
+### TILEGRAM SF? 
+
+
+
+#### FINE TUNNING Y COMPOSICION
